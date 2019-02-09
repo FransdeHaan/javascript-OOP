ba6048f024fec50b9fea241aa5503faea5c00a52
Frans de Haan <f.pdehaan1992@gmail.com>
2019-02-09 14:24:09 +0100

    Functions are Objects

--- index.js
+++ index.js
@@ -70,3 +70,19 @@ function Circle(radius) {
 }
 
 const another = new Circle(1); */
+
+/**************************************
+ * 6-Functions are Objects
+ */
+
+/* function Circle(radius) {
+  this.radius = radius;
+  this.draw = function () {
+    console.log('draw');
+  }
+}
+
+Circle.call({}, 1);
+Circle.apply({}, [1, 2, 3]);
+
+const another = new Circle(1); */
