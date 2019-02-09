ed4f4eea8eac066d6d6544e588872b8e564b2807
Frans de Haan <f.pdehaan1992@gmail.com>
2019-02-09 10:56:55 +0100

    Factory function

--- index.js
+++ index.js
@@ -2,15 +2,31 @@
  * 2-Object Literals
  */
 
-const circle = {
-    radius: 1,
-    location: {
-        x: 1,
-        y: 1
-    },
-    draw: function() {
-        console.log('draw');
-    }
+/* const circle = {
+  radius: 1,
+  location: {
+    x: 1,
+    y: 1
+  },
+  draw: function () {
+    console.log('draw');
+  }
 };
 
+circle.draw(); */
+
+/**************************************
+ * 3-Factory Function
+ */
+
+function createCircle(radius) {
+  return {
+    radius,
+    draw: function () {
+      console.log('draw');
+    }
+  };
+}
+
+const circle = createCircle(1);
 circle.draw();
