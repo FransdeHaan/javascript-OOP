856f9e40cfbe9b9d15f4db89d104904d6fa85a85
Frans de Haan <f.pdehaan1992@gmail.com>
2019-02-13 20:30:42 +0100

    Adding or Removing Properties

--- index.js
+++ index.js
@@ -99,3 +99,23 @@ function increase(obj) {
 
 increase(obj);
 console.log(obj); */
+
+/**************************************
+ * 8-Adding or Removing Properties
+ */
+
+/* function Circle(radius) {
+  this.radius = radius;
+  this.draw = function () {
+    console.log('draw');
+  }
+}
+
+const circle = new Circle(10);
+
+circle.location = { x: 1 };
+
+const propertyName = 'location';
+circle[propertyName] = { x: 1 };
+
+delete circle.location; */
