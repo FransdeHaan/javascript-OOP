57694c4918bdd84e1785f5b2468df164dafca54e
Frans de Haan <f.pdehaan1992@gmail.com>
2019-02-19 19:39:36 +0100

    Abstraction

--- index.js
+++ index.js
@@ -143,3 +143,25 @@ console.log(keys);
 
 if ('radius' in circle)
   console.log('Circle has a radius.'); */
+
+/**************************************
+ * 10-Abstraction
+ */
+
+/* function Circle(radius) {
+  this.radius = radius;
+
+  this.defaultLocation = { x: 0, y: 0 };
+
+  this.computeOptimumLocation - function () {
+
+  }
+
+  this.draw = function () {
+    this.computeOptimumLocation();
+
+    console.log('draw');
+  }
+}
+
+const circle = new Circle(10); */
